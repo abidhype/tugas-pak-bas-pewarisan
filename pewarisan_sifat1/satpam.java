1ce6604081070f0de8b4da6f90e2eb5d1216dea1
abidhype <sakhariyanto001@gmail.com>
2025-08-28 10:55:08 +0800

    Add files via upload

---
+++ pewarisan_sifat1/satpam.java
@@ -0,0 +1,18 @@
+package pewarisan_sifat1;
+
+public class satpam extends pegawai{
+
+	@Override
+	public void menampilkan()
+	{
+		System.out.println("Nama : " + nama);
+		System.out.println("Id Pegawai : " + id_pegawai);
+		System.out.println("Gaji : " + gaji);
+	}
+
+	public void tugas(){
+		System.out.println("Tugas : Menjaga keamanan didalam dan diluar frenchise");
+		System.out.println("----------------------------------------------");
+	}
+
+}
